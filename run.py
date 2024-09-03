cce45ba334056fa1a19afdcc4ba91755849253cb
Dhruvin Shah <dhruvin3@gmail.com>
2024-09-02 20:30:13 -0400

    refactor: remove unused logging
    The logging module and the redundant log level setup have been removed in favor of using loguru for logging.

--- run.py
+++ run.py
@@ -1,17 +1,10 @@
 """Run the sync between Jellyfin and Plex."""
 
-import logging
-
 from loguru import logger
 
 from syncerr.api.jellyfin import Jellyfin
 from syncerr.config import cfg
 from syncerr.engine import HttpEngine
-
-try:
-    LEVEL = getattr(logging, str.upper(cfg.LOG_LEVEL))
-except AttributeError:
-    LEVEL = logging.INFO
 
 logger.info("Running...")
 
